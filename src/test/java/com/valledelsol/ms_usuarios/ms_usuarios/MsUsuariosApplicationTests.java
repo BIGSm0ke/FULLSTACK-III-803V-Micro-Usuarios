394d4c16ace15043d0407841b045a9f82825d632
JOOORMUNDGANDER <marcelo.sade.palma@gmail.com>
2026-04-26 22:10:23 -0400

    Version SpringBoot

---
+++ src/test/java/com/valledelsol/ms_usuarios/ms_usuarios/MsUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valledelsol.ms_usuarios.ms_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
